49a5df1a9a28cd5aa29f4e4f5269f75190f61464
wth <phi.wth@gmail.com>
2019-07-21 14:47:11 +0800

    update

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,6 +12,9 @@
 #
 import os
 import sys
+import mock
+MOCK_MODULES = ['numpy', 'scipy', 'panda3d', 'cvxopt', 'matplotlib', 'matplotlib.pyplot']
+
 sys.path.insert(0, os.path.abspath('../../src'))
 
 master_doc = 'index'
